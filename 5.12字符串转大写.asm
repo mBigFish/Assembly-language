afc869b4c72bdca0dfe5ef292cd367f39d806064
mBigFish <3433148732@qq.com>
2023-12-08 11:23:20 +0800

    输出hello world

---
+++ 5.12字符串转大写.asm
@@ -0,0 +1,36 @@
+assume ds:data, ss:stack, ds:data
+
+data segment
+	str db 'hello world', '$'
+data ends
+
+stack segment
+	db 10 dup(0) 
+stack ends
+
+code segment
+	start:	
+	mov ax, data
+	mov ds, ax
+	mov bx, 0
+	mov cx, 11
+
+;-----------------------------
+;a -> 61h -> 0110 0001 
+;A -> 41h -> 0100 0001
+;0110 0001
+;1101 1111
+;and
+;0100 0001
+;-----------------------------
+	s:
+		mov al, ds:[bx]
+		and al, 11011111b
+		mov ds:[bx], al
+		inc bx
+		loop s
+
+	mov al, 4cH
+	int 21h
+code ends
+end start
--- t2.asm
+++ t2.asm
@@ -7,7 +7,32 @@ stack segment
 stack ends
 codesg SEgment
 	start:	
-		mov ax,data
-		mov ds,ax
+	mov ax,data
+	mov ds,ax
+	
+	mov bx,0
+	mov cx,11
+	s:
+		mov al,[bx]
+		and al,1011111B
+		mov [bx],al
+		inc bx
+		loop s
+		
+		;mov dx,offset str
 		lea dx,str		
 		mov ah,9
+		int 21H
+
+		mov ah,4cH
+		int 21H
+codesg ends
+end start
+
+
+
+
+
+
+
+
